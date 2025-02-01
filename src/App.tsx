286ea6c2350be9df11991b76228baa5f06936c9f
Guy <goldenrodge99@gmail.com>
2025-02-01 16:30:48 +0500

    Bismillah

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-full text-[35px]/[35px] text-red-500">
+      Easy Forms
+    </div>
+  );
+}
+
+export default App;
